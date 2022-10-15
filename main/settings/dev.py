21014407714a744a35cf71efb34e48e4cf2e3e85
mehmet-turkeri <mehmetturkeri51@gmail.com>
2022-10-15 18:03:40 +0200

    up

--- main/settings/dev.py
+++ main/settings/dev.py
@@ -17,10 +17,46 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-# SQLite
+# LOGS:
+LOGGING = { 
+    "version": 1, 
+    "disable_existing_loggers": True, 
+    "formatters": { 
+        "standard": { 
+            "format": "[%(levelname)s] %(asctime)s %(name)s: %(message)s" 
+        }, 
+        'detail': { 
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}', 
+            'style': '{', 
+        },
+    }, 
+    "handlers": { 
+        "console": { 
+            "class": "logging.StreamHandler", 
+            "formatter": "standard", 
+            "level": "ERROR", 
+            "stream": "ext://sys.stdout", 
+            }, 
+        'file': { 
+            'class': 'logging.FileHandler', 
+            "formatter": "detail", 
+            'filename': './dev_logs.log', 
+            'level': 'WARNING',
+        }, 
+    }, 
+    "loggers": { 
+        "django": { 
+            "handlers": ["console", 'file'], 
+            "level": config("DJANGO_LOG_LEVEL", "WARNING"), 
+            'propagate': True, 
+        }, 
+    }, 
+}
+
+# SQLite:
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dev_db.sqlite3',
     }
 }
